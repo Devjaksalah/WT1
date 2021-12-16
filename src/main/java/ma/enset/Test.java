71ac02b8de6db022f71e616d48bc85358df827d9
andrii piatakha <andrii.piatakha@exemple.com>
2021-12-16 14:08:39 +0100

    Patch code

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,10 +1,12 @@
 /**
  * Comment
+ * Line 2 Mohammed ENSET
  */
 package ma.enset;
 public class Test{
     public static void main(String[]args){
         //---------------------------------
-        System.out.println("Hello!");
+        String message="Hello!";
+        System.out.println(message);
     }
 }
